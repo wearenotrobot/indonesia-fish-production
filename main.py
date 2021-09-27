b39f6a847e1559ecb209dfbd4faeafb96d02e7a1
mizantoyyibun <untirta.mizan62@gmail.com>
2021-09-28 06:11:52 +0700

    initial commit

--- main.py
+++ main.py
@@ -3,24 +3,9 @@ Total fish production in Indonesia
 Data collected from KKP.go.id
 """
 
-
 def data_extraction():
-    """
-    PRODUKSI TAHUN 2021
-    Nama Jenis Ikan	Total Produksi (Kg)
-    Cakalang [SKJ]              : 70.031.295,89
-    Layang Benggol              : 34.311.048,90
-    Madidihang [YFT]            : 34.071.561,50
-    Cumi-Cumi                   : 29.018.893,30
-    Layang Deles                : 16.432.081,10
-    Swanggi                     : 13.538.254,47
-    Lemuru                      : 13.470.370,00
-    Tongkol Pisang-Cerutu [BLT] : 11.465.453,02
-    Siro                        : 7.362.794,00
-    Tongkol Pisang-Balaki [FRI]	: 6.977.063,31
-    :return:
-    """
     hasil = dict()
+    hasil['nama_jenis_ikan'] = 'Total Produksi (Kg)'
     hasil['cakalang_[SKJ]'] = '70.031.295,89'
     hasil['layang_benggol'] = '34.311.048,90'
     hasil['madidihang_[YFT]'] = '34.071.561,50'
@@ -36,7 +21,18 @@ def data_extraction():
 
 
 def show_data(result):
-    pass
+    print('INDONESIA FISH PRODUCTION IN 2021')
+    print(f"Nama Jenis Ikan {result['nama_jenis_ikan']}")
+    print(f"Cakalang [SKJ] {result['cakalang_[SKJ]']}")
+    print(f"layang_benggol {result['layang_benggol']}")
+    print(f"Madidihang [YFT] {result['madidihang_[YFT]']}")
+    print(f"Cumi-Cumi {result['cumi-cumi']}")
+    print(f"Layang Deles {result['layang_deles']}")
+    print(f"Swanggi {result['swanggi']}")
+    print(f"Lemuru {result['lemuru']}")
+    print(f"Tongkol Pisang-Cerutu [BLT] {result['tongkol_pisang-cerutu_[BLT]']}")
+    print(f"Siro {result['swanggi']}")
+    print(f"Tongkol Pisang-Balaki [FRI] {result['tongkol_pisang-balaki_[FRI]']}")
 
 
 if __name__ == '__main__':
