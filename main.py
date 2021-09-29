07e676bdac834cb2ecb4265c49f16aec1b420059
mizantoyyibun <untirta.mizan62@gmail.com>
2021-09-29 10:23:50 +0700

    initial commit

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ Data collected from KKP.go.id
 import produksiikan
 
 if __name__ == '__main__':
-    print('Apikasi Utama')
+    print('Aplikasi Utama')
     result = produksiikan.data_extraction()
     produksiikan.show_data(result)
--- produksiikan/__init__.py
+++ produksiikan/__init__.py
@@ -1,22 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
 
 def data_extraction():
-    hasil = dict()
-    hasil['nama_jenis_ikan'] = 'Total Produksi (Kg)'
-    hasil['cakalang_[SKJ]'] = '70.031.295,89'
-    hasil['layang_benggol'] = '34.311.048,90'
-    hasil['madidihang_[YFT]'] = '34.071.561,50'
-    hasil['cumi-cumi'] = '29.018.893,30'
-    hasil['layang_deles'] = '16.432.081,10'
-    hasil['swanggi'] = '13.538.254,47'
-    hasil['lemuru'] = '13.470.370,00'
-    hasil['tongkol_pisang-cerutu_[BLT]'] = '11.465.453,02'
-    hasil['siro'] = '7.362.794,00'
-    hasil['tongkol_pisang-balaki_[FRI]'] = '6.977.063,31'
+    try:
+        content = requests.get('http://pipp.djpt.kkp.go.id/produksi_harga')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        title = soup.find('title')
+        print(title.string) # pakai string supaya tag nya hilang
+        # print(soup.prettify())
 
-    return hasil
+        hasil = dict()
+        hasil['nama_jenis_ikan'] = 'Total Produksi (Kg)'
+        hasil['cakalang_[SKJ]'] = '70.031.295,89'
+        hasil['layang_benggol'] = '34.311.048,90'
+        hasil['madidihang_[YFT]'] = '34.071.561,50'
+        hasil['cumi-cumi'] = '29.018.893,30'
+        hasil['layang_deles'] = '16.432.081,10'
+        hasil['swanggi'] = '13.538.254,47'
+        hasil['lemuru'] = '13.470.370,00'
+        hasil['tongkol_pisang-cerutu_[BLT]'] = '11.465.453,02'
+        hasil['siro'] = '7.362.794,00'
+        hasil['tongkol_pisang-balaki_[FRI]'] = '6.977.063,31'
+        return hasil
+    else:
+        return None
 
 
 def show_data(result):
+    if result is None:
+        print('Tidak bisa menemukan data')
+    return None
     print('INDONESIA FISH PRODUCTION IN 2021')
     print(f"Nama Jenis Ikan {result['nama_jenis_ikan']}")
     print(f"Cakalang [SKJ] {result['cakalang_[SKJ]']}")
