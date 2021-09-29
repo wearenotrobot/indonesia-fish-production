fc89c281d7c3508c46adcf31d26e54281ce2558b
mizantoyyibun <untirta.mizan62@gmail.com>
2021-09-29 15:18:35 +0700

    initial commit

--- produksiikan/__init__.py
+++ produksiikan/__init__.py
@@ -10,21 +10,80 @@ def data_extraction():
     if content.status_code == 200:
         soup = BeautifulSoup(content.text, 'html.parser')
         title = soup.find('title')
-        print(title.string) # pakai string supaya tag nya hilang
-        # print(soup.prettify())
+        print(title.string)  # pakai string supaya tag nya hilang
+        result = soup.find('table', {'class', 'table table-striped'})
+        result = result.findChildren('td')
+
+        i = 0
+
+        for res in result:
+            print(i, res)
+            if i == 0:
+                tf1 = res.text
+            elif i == 1:
+                pr1 = res.text
+            elif i == 2:
+                tf2 = res.text
+            elif i == 3:
+                pr2 = res.text
+            elif i == 4:
+                tf3 = res.text
+            elif i == 5:
+                pr3 = res.text
+            elif i == 6:
+                tf4 = res.text
+            elif i == 7:
+                pr4 = res.text
+            elif i == 8:
+                tf5 = res.text
+            elif i == 9:
+                pr5 = res.text
+            elif i == 10:
+                tf6 = res.text
+            elif i == 11:
+                pr6 = res.text
+            elif i == 12:
+                tf7 = res.text
+            elif i == 13:
+                pr7 = res.text
+            elif i == 14:
+                tf8 = res.text
+            elif i == 15:
+                pr8 = res.text
+            elif i == 16:
+                tf9 = res.text
+            elif i == 17:
+                pr9 = res.text
+            elif i == 18:
+                tf10 = res.text
+            elif i == 19:
+                pr10 = res.text
+            i = i + 1
 
         hasil = dict()
-        hasil['nama_jenis_ikan'] = 'Total Produksi (Kg)'
-        hasil['cakalang_[SKJ]'] = '70.031.295,89'
-        hasil['layang_benggol'] = '34.311.048,90'
-        hasil['madidihang_[YFT]'] = '34.071.561,50'
-        hasil['cumi-cumi'] = '29.018.893,30'
-        hasil['layang_deles'] = '16.432.081,10'
-        hasil['swanggi'] = '13.538.254,47'
-        hasil['lemuru'] = '13.470.370,00'
-        hasil['tongkol_pisang-cerutu_[BLT]'] = '11.465.453,02'
-        hasil['siro'] = '7.362.794,00'
-        hasil['tongkol_pisang-balaki_[FRI]'] = '6.977.063,31'
+        # hasil['nama_jenis_ikan'] = ikan0
+        # hasil['total_produksi'] =
+
+        hasil['type_fish1'] = tf1
+        hasil['production1'] = pr1
+        hasil['type_fish2'] = tf2
+        hasil['production2'] = pr2
+        hasil['type_fish3'] = tf3
+        hasil['production3'] = pr3
+        hasil['type_fish4'] = tf4
+        hasil['production4'] = pr4
+        hasil['type_fish5'] = tf5
+        hasil['production5'] = pr5
+        hasil['type_fish6'] = tf6
+        hasil['production6'] = pr6
+        hasil['type_fish7'] = tf7
+        hasil['production7'] = pr7
+        hasil['type_fish8'] = tf8
+        hasil['production8'] = pr8
+        hasil['type_fish9'] = tf9
+        hasil['production9'] = pr9
+        hasil['type_fish10'] = tf10
+        hasil['production10'] = pr10
         return hasil
     else:
         return None
@@ -33,16 +92,17 @@ def data_extraction():
 def show_data(result):
     if result is None:
         print('Tidak bisa menemukan data')
-    return None
+
     print('INDONESIA FISH PRODUCTION IN 2021')
-    print(f"Nama Jenis Ikan {result['nama_jenis_ikan']}")
-    print(f"Cakalang [SKJ] {result['cakalang_[SKJ]']}")
-    print(f"layang_benggol {result['layang_benggol']}")
-    print(f"Madidihang [YFT] {result['madidihang_[YFT]']}")
-    print(f"Cumi-Cumi {result['cumi-cumi']}")
-    print(f"Layang Deles {result['layang_deles']}")
-    print(f"Swanggi {result['swanggi']}")
-    print(f"Lemuru {result['lemuru']}")
-    print(f"Tongkol Pisang-Cerutu [BLT] {result['tongkol_pisang-cerutu_[BLT]']}")
-    print(f"Siro {result['swanggi']}")
-    print(f"Tongkol Pisang-Balaki [FRI] {result['tongkol_pisang-balaki_[FRI]']}")
+    # print(f"Nama Jenis Ikan {result['nama_jenis_ikan']}")
+    print(result['type_fish1'], result['production1'])
+    print(result['type_fish2'], result['production2'])
+    print(result['type_fish3'], result['production3'])
+    print(result['type_fish4'], result['production4'])
+    print(result['type_fish5'], result['production5'])
+    print(result['type_fish6'], result['production6'])
+    print(result['type_fish7'], result['production7'])
+    print(result['type_fish8'], result['production8'])
+    print(result['type_fish9'], result['production9'])
+    print(result['type_fish10'], result['production10'])
+
